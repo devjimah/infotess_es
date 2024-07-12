b7157b9ad1b3382c036c6333783c1e5b66dd8810
Jimah Abraham Zorwi <devjimah@gmail.com>
2024-07-12 03:10:58 +0000

    chore: Add loading spinner to Login component during login process

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import { Button, Input, Form, message } from "antd";
+import { Button, Input, Form, message, Spin } from "antd";
 import { useAppContext } from "../context/AppProvider";
+import { LoadingOutlined } from "@ant-design/icons";
 import "tailwindcss/tailwind.css";
 import { useState } from "react";
 import axios from "axios";
@@ -10,8 +11,10 @@ function Login() {
   const navigate = useNavigate();
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
+    setLoading(true);
     try {
       const formData = { userName, password };
       const response = await axios.post(
@@ -28,63 +31,72 @@ function Login() {
       const token = data.token;
       localStorage.setItem("token", token);
       setIsLoggedIn(true);
+
       message.success("Login successful");
       navigate("/dashboard");
     } catch (error) {
       console.log(error);
+      message.error("Login failed");
+    } finally {
+      setLoading(false);
     }
-    // const { name, password } = values;
-    // if (name === "admin" && password === "password") {
-    //   localStorage.setItem("isLoggedIn", "true");
-    //   setIsLoggedIn(true);
-    //   message.success("Login successful");
-    //   navigate("/dashboard");
-    // } else {
-    //   message.error("Invalid credentials");
-    // }
   };
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="bg-white p-8 rounded shadow-lg max-w-md w-full flex flex-col items-center">
         <h2 className="text-xl font-bold uppercase mb-4">hod/patron Login</h2>
-        <Form
-          name="login"
-          onFinish={handleLogin}
-          className="w-full flex flex-col items-center"
+        <Spin
+          spinning={loading}
+          indicator={
+            <LoadingOutlined
+              style={{
+                fontSize: 48,
+              }}
+              spin
+            />
+          }
         >
-          <Form.Item
-            name="userName"
-            rules={[{ required: true, message: "Please input your name!" }]}
+          <Form
+            name="login"
+            onFinish={handleLogin}
+            className="w-full flex flex-col items-center"
           >
-            <Input
-              type="text"
+            <Form.Item
               name="userName"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              placeholder="Name"
-              className="w-48 py-2 border-b-2 border-blue-950 focus:outline-none focus:border-b-2 focus:border-blue-950"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password
-              type="password"
+              rules={[{ required: true, message: "Please input your name!" }]}
+            >
+              <Input
+                type="text"
+                name="userName"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                placeholder="Name"
+                className="w-48 py-2 border-b-2 border-blue-950 focus:outline-none focus:border-b-2 focus:border-blue-950"
+              />
+            </Form.Item>
+            <Form.Item
               name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              className="w-48 py-2 border-b-2 border-blue-950 focus:outline-none focus:border-b-2 focus:border-blue-950"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="w-48">
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                className="w-48 py-2 border-b-2 border-blue-950 focus:outline-none focus:border-b-2 focus:border-blue-950"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="w-48">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Spin>
       </div>
     </div>
   );
